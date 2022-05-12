0d4d8ff78186eb2e5ee438fbb4da1200e0b0399d
dxkkxn <ybkstudies@gmail.com>
2022-05-12 22:28:14 +0200

    dijkstra pep8 modifs

--- dijkstra.py
+++ dijkstra.py
@@ -6,6 +6,7 @@ import bisect
 
 inf = float('inf')
 
+
 def cost(grid, v1i: int, v1j: int, v2i: int, v2j: int):
     """Cost to go from v1 vertex to v2 vertex."""
     # we detect a diagonal mouvement
@@ -15,11 +16,10 @@ def cost(grid, v1i: int, v1j: int, v2i: int, v2j: int):
         weight = (grid[v1i][v1j] + grid[v2i][v2j]) / 2
     return weight
 
+
 def get_path(prev: dict, start: tuple, target: tuple):
-    """
-    get the vertex path to go from start to target using prev dict
-    """
-    curr_node= target
+    """Get path of vertexes going from start to target using prev dict."""
+    curr_node = target
     path = [target]
     while curr_node != start:
         path.append(prev[curr_node])
@@ -31,6 +31,8 @@ def get_path(prev: dict, start: tuple, target: tuple):
 def check_neighbours_sd(grid, i: int, j: int, weight: dict,
                         sorted_weight: SortedDict, prev: dict, mark: set):
     """
+    Check neighbours using sorted dict.
+
     for each neighbour of i, j if a new path is found update the parameters
     (weight, sorted_weight, prev, mark)
     """
@@ -79,8 +81,15 @@ def check_neighbours(grid, i: int, j: int, weight: dict, weight_heap: list,
                 prev[(ni, nj)] = (i, j)
     return
 
+
 def check_neighbours_dq(grid, i: int, j: int, weight: dict,
                         weight_dq: deque, prev: dict, mark: set):
+    """
+    Check neighbours using deque.
+
+    for each neighbour of i, j if a new path is found update the parameters
+    (weight, weight_heap, prev, mark)
+    """
     len_l = len(grid)
     len_c = len(grid[0])
     dep = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0),
@@ -100,10 +109,7 @@ def check_neighbours_dq(grid, i: int, j: int, weight: dict,
 
 def init_all_vertex(grid, weight: dict, weight_heap: list,
                     start: tuple):
-    """
-    Initialition of all vertex weight to +infinity except start vertex,
-    which is initialized to 0
-    """
+    """Initialition of all vertex weight to +infinity except start vertex."""
     len_l = len(grid)
     len_c = len(grid[0])
     for i in range(len_l):
@@ -114,12 +120,10 @@ def init_all_vertex(grid, weight: dict, weight_heap: list,
     heapq.heappush(weight_heap, (0, start))
     return
 
+
 def init_all_vertex_sd(grid, weight: dict,
                        sorted_weight: SortedDict, start: tuple):
-    """
-    Initialition of all vertex weight to +infinity except start vertex,
-    which is initialized to 0
-    """
+    """Initialition of all vertex weight to +infinity except start vertex."""
     len_l = len(grid)
     len_c = len(grid[0])
     sorted_weight[inf] = set()
@@ -132,16 +136,15 @@ def init_all_vertex_sd(grid, weight: dict,
     weight[start] = 0
     return
 
+
 def init_all_vertex_deque(grid, weight: dict, weight_deque: deque,
                           start: tuple):
-    """
-    Initialisation of all vertex weight to +infinity
-    """
+    """Initialise of all vertex weight to +infinity."""
     len_l = len(grid)
     len_c = len(grid[0])
     for i in range(len_l):
         for j in range(len_c):
-            weight_deque.append((inf, (i,j)))
+            weight_deque.append((inf, (i, j)))
             weight[(i, j)] = inf
     weight_deque.remove((inf, start))
     weight_deque.appendleft((0, start))
@@ -150,6 +153,7 @@ def init_all_vertex_deque(grid, weight: dict, weight_deque: deque,
 
 
 def dijkstra_matrix_heap(grid, start: tuple, target: tuple):
+    """Dijkstra algorithm using a heap and graph being a matrix."""
     weight_heap = []
     weight = {}
     mark = set()
@@ -165,7 +169,9 @@ def dijkstra_matrix_heap(grid, start: tuple, target: tuple):
         check_neighbours(grid, curr_i, curr_j, weight, weight_heap, prev, mark)
     return get_path(prev, start, target)
 
+
 def dijkstra_matrix_deque(grid, start: tuple, target: tuple):
+    """Dijkstra algorithm using a deque and graph being a matrix."""
     weight_deque = deque()
     weight = {}
     mark = set()
@@ -182,7 +188,9 @@ def dijkstra_matrix_deque(grid, start: tuple, target: tuple):
         check_neighbours_dq(grid, curr_i, curr_j, weight, weight_deque, prev, mark)
     return get_path(prev, start, target)
 
+
 def dijkstra_matrix_sorted_dict(grid, start: tuple, target: tuple):
+    """Dijkstra algorithm using a sorted dict and graph being a matrix."""
     sorted_weight = SortedDict()
     weight = {}
     prev = {}
